8795155db76d7c5d249240b1f225d3d2c481571a
SergA174 <116829429+SergA174@users.noreply.github.com>
2022-11-16 03:54:16 +0500

    Задача 25:Принимает на вход два числа (A и B)и возводит число A в натуральную степень B

---
+++ Task_25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+Console.Write("Введите целое число A: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число B: ");
+int y = Convert.ToInt32(Console.ReadLine());
+int count = y;
+int z = 1;
+void DegNumbers(int z) 
+{
+    for (int i = 0; i <= y-1; i++)
+        z = z * x;
+    Console.WriteLine($"Число -A- возведенное в натуральную степень -B- ровно: {z}");
+}
+DegNumbers(z);
+
